7b016efbd0b4ecc70e86b375cb34e8ef37e7c2a8
Meinolf Ottensmann <meinolf.ottensmann@web.de>
2019-07-18 16:28:44 +0200

    Quick way to compare data frames. Call in Rmarkdown

---
+++ R/compare.df.R
@@ -0,0 +1,19 @@
+#' Compare DFs
+#'
+#' @param df.new data frame
+#' @param df.old data frame
+#' @param by grouping column
+#' @import magrittr
+#' @export
+#'
+compare.df <- function(df.new = NULL, df.old = NULL, by = NULL) {
+  df.new <- readxl::read_xlsx(df.new)
+  df.old <- readxl::read_xlsx(df.old)
+  df.new <- df.old[, names(df.old)]
+  out <- compareDF::compare_df(df_old = df_old,
+                               df_new =  df_new,
+                               group_col = by, limit_html = 5000)
+  knitr::knit_print(out$html_output)
+}
+
+
