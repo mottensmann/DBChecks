edb2b9c17590584aba8f1b997c456a3583c2a1ff
Meinolf Ottensmann <meinolf.ottensmann@web.de>
2019-07-19 17:32:43 +0200

    Update

--- R/terr.centroid.R
+++ R/terr.centroid.R
@@ -2,31 +2,34 @@
 #'
 #' @param name Territory name
 #' @param df data frame
-#' @param weighted Logical. By default weighted mean of all breeding attempts
+#' @param weighted Logical. By default only considers unique nests
 #' @return Centroid lon and lat coordinates
 #' @import magrittr
 #' @export
 #'
-terr.centroid <- function(name = NULL, df = buzzard_db[["repro_fledge_db"]], weighted = T) {
+terr.centroid <- function(name = NULL, df = buzzard_db[["repro_fledge_db"]], weighted = F) {
   if (is.null(name)) stop("Specify a Territory")
   if (name %in% df[["Territory"]]) {
     out <- dplyr::filter(df, Territory == name, !is.na(E), !is.na(N))[,c("E",  "N")]
+    # rename N, E to lat and long
+    names(out) <- c("Long", "Lat")
+    if (isFALSE(weighted)) out <- unique.data.frame(out)
     if (nrow(out) < 4) {
-      coords <- data.frame(centroid.lat = out[["N"]][1],
-                           centroid.long = out[["E"]][1])
-    } else if (unique(out[["N"]]) %>% length == 1) {
-      coords <- data.frame(centroid.lat = out[["N"]][1],
-                           centroid.long = out[["E"]][1])
-    } else if (unique(out[["E"]]) %>% length == 1) {
-      coords <- data.frame(centroid.lat = out[["N"]][1],
-                           centroid.long = out[["E"]][1])
+      coords <- data.frame(centroid.lat = out[["Lat"]][1],
+                           centroid.long = out[["Long"]][1])
+    } else if (unique(out[["Lat"]]) %>% length == 1) {
+      coords <- data.frame(centroid.lat = out[["Lat"]][1],
+                           centroid.long = out[["Long"]][1])
+    } else if (unique(out[["Long"]]) %>% length == 1) {
+      coords <- data.frame(centroid.lat = out[["Lat"]][1],
+                           centroid.long = out[["Long"]][1])
     } else if (unique.data.frame(out) %>% nrow > 3) {
-      coords <- geosphere::centroid(out[,c("E", "N")]) %>%
+      coords <- geosphere::centroid(out[,c("Long", "Lat")]) %>%
         .[2:1] %>%
         set_names(x = ., value =  c("centroid.lat", "centroid.long"))
     } else {
-      coords <- data.frame(centroid.lat = out[["N"]][1],
-                           centroid.long = out[["E"]][1])
+      coords <- data.frame(centroid.lat = out[["Lat"]][1],
+                           centroid.long = out[["Long"]][1])
     }
   } else {
     stop("Territory", name, "does not exist")
@@ -35,5 +38,5 @@ terr.centroid <- function(name = NULL, df = buzzard_db[["repro_fledge_db"]], wei
 }
 # load("../../01-PhD/00-Raw/RData/buzzard_db.RData")
 # library(magrittr)
-# name <- "Dreck"
+# name <- "Wittenbrock III"
 # df = buzzard_db[["repro_fledge_db"]]
