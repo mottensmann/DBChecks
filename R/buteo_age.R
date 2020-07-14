cf87660f3451dbddd10903fda7cd7ef016898b10
Meinolf Ottensmann <meinolf.ottensmann@web.de>
2020-07-14 17:45:13 +0200

    changed to polynomial fit

--- R/buteo_age.R
+++ R/buteo_age.R
@@ -1,13 +1,17 @@
 #' Estimate the age of juvenile buzzards based on wing length
 #'
 #' @description Buzzard age is estimated based on a standard growth curve modeled from published data by
-#' Bijlsma (Limosa 72, 1999)
+#' Bijlsma (Limosa 72, 1999).
+#'
+#' @details Estimations are based on a four degree polynomial fit
 #'
 #' @param df data frame containing data
 #' @param wing column name for wing measurements
 #' @param sex column name giving the sex of individuals or NULL
 #' @param unit unit of values. By default in mm
 #' @param .plot logical
+#' @param .show_model logical
+#' @param decimals decimals
 #'
 #' @references
 #' Bijlsma, RG 1999: Sex determination of nestling Common Buzzards Buteo buteo. Limosa 72, 1.10
@@ -21,7 +25,8 @@
 #'
 #' @export
 #'
-buteo_age <- function(df = NULL, wing = "wing", sex = "sex", unit = c("mm", "cm"), .plot = F) {
+buteo_age <- function(df = NULL, wing = "wing", sex = "sex", unit = c("mm", "cm"), .plot = F, decimals = 2,
+                      .show_model = T) {
   unit <- match.arg(unit)
 
   ## translate binary sex
@@ -43,14 +48,18 @@ buteo_age <- function(df = NULL, wing = "wing", sex = "sex", unit = c("mm", "cm"
     data[["sd"]] <- data[["sd"]]/10
   }
 
-  ## build model
+  ## build models
+
   if (is.null(sex)) {
-    model <- stats::lm(age ~ mean, data)
+    #model <- stats::lm(age ~ mean, data)
+    model <-  stats::lm(data$age ~ poly(data$mean, 4, raw = T))
     fit.val <- data.frame(age = stats::predict(model, data), mean = data[["mean"]])
   } else {
-    model <- stats::lm(age ~ mean + sex, data)
+    #model <- stats::lm(age ~ mean + sex, data)
+    model <-  stats::lm(data$age ~ poly(data$mean, 4, raw = T) + data$sex)
     fit.val <- data.frame(age = stats::predict(model, data), mean = data[["mean"]], sex = data[["sex"]])
   }
+  if (isTRUE(.show_model)) print(model)
 
   ## plot
   if (.plot == T) {
@@ -65,15 +74,36 @@ buteo_age <- function(df = NULL, wing = "wing", sex = "sex", unit = c("mm", "cm"
                          breaks = c("1", "0"),
                          labels = c("Male", "Female")) +
     ggtitle("Common Buzzard growth curve",
-            subtitle = "data: Bijlsma, RG (1999): Limosa 72") +
-    annotate("text", y = 50, x = ifelse(unit == "mm", 70, 7), colour = "red",
-             label = paste0("y=", round(coefficients(model)[[2]],2), "*x+",
-                            round(coefficients(model)[[1]] - coefficients(model)[[3]],2),
-                            " , R²=0.99")) +
-    annotate("text", y = 47, x = ifelse(unit == "mm", 70, 7), colour = "blue",
-             label = paste0("y=", round(coefficients(model)[[2]],2), "*x+",
-                            round(coefficients(model)[[1]],2),
-                            " , R²=0.99"))
+            subtitle = "data: Bijlsma, RG (1999): Limosa 72")
+
+    if (!is.null(sex)) {
+      plot <- plot +
+      annotate("text", y = 50, x = ifelse(unit == "mm", 70, 7), colour = "red",
+               label = paste0("f(wing)=",
+                              "3.647e-01*x-",
+                              "2.211e-03*x^2+",
+                              "7.472e-06*x^3-",
+                              "8.162e-09*x^4-",
+                              "5.078e+00")) +
+
+        annotate("text", y = 47, x = ifelse(unit == "mm", 70, 7), colour = "blue",
+                 label = paste0("f(wing)=",
+                                "3.647e-01*x-",
+                                "2.211e-03*x^2+",
+                                "7.472e-06*x^3-",
+                                "8.162e-09*x^4-",
+                                "4.2263"))
+    } else {
+      plot <- plot +
+        annotate("text", y = 50, x = ifelse(unit == "mm", 70, 7), colour = "red",
+                 label = paste0("f(wing)=",
+                                "3.689e-01*x-",
+                                "2.271e-03*x^2+",
+                                "7.771e-06*x^3-",
+                                "8.643e-09*x^4-",
+                                "4.725e+00"))
+    }
+
 
   out <- plot
   } else {
@@ -93,6 +123,15 @@ buteo_age <- function(df = NULL, wing = "wing", sex = "sex", unit = c("mm", "cm"
     }
     out <- stats::predict.lm(model, to_predict, se.fit = T)
   }
+
   return(out)
 }
 
+# rm(list = ls())
+# df = data.frame(wing = c(240,240), sex = c("male","female"))
+# wing = "wing"
+# sex = "sex"
+# unit = "cm"
+# .plot = F
+# decimals = 2
+# .show_model = T
